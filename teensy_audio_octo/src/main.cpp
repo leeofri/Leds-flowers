e00dd3bd9e33a446d98ec5e35daa8fb88e0cb67e
leeo <leeo@wix.com>
2022-09-03 12:40:25 +0300

    cahnge to 3.5

--- teensy_audio_octo/src/main.cpp
+++ teensy_audio_octo/src/main.cpp
@@ -21,17 +21,17 @@
 
 // The display size and color to use
 const unsigned int matrix_width = 60;
-const unsigned int matrix_height = 32;
+const unsigned int matrix_height = 40;
 
 // max HSV color ( Blink )
 const float FadeMaxH = 360;
 const float FadeMaxS = 100;
-const float FadeMaxV = 60;
+const float FadeMaxV = 100;
 
 // min HSV color ( Solid / base color )
-const float FadeMinH = 60;
+const float FadeMinH = 30;
 const float FadeMinS = 100;
-const float FadeMinV = 10;
+const float FadeMinV = 6;
 
 // current HSV color
 float currH = FadeMinH;
@@ -41,21 +41,24 @@ float currV = FadeMinV;
 const unsigned int baseVal = 15;
 const unsigned int MyBlinkColor = HSVtoRGB(FadeMaxH, FadeMaxS, FadeMaxV); // 0xe60073; // makeColor(330, 100, maxVal);//;
 const unsigned int myBaseColor = HSVtoRGB(FadeMinH, FadeMinS, FadeMinV);  // 0x000000;// 0x1a000d;//makeColor(330, 100, baseVal);//0x1a000d;
-float colorFadeFactor = 1.0;                                              // 1 is none
-float colorChangeFactor = 50;                                             // 1 is none
+float colorFadeFactor = 1.0;                                              // 1 is none                                        // 1 is none
 
 // These parameters adjust the vertical thresholds
-const float maxLevel = 0.06;     // 1.0 = max, lower is more "sensitive"
+float maxLevel = 0.001;     // 1.0 = max, lower is more "sensitive"
 const float dynamicRange = 40.0; // total range to display, in decibels
-const float linearBlend = 0.3;   // useful range is 0 to 0.7
+const float linearBlend = 0.5;   // useful range is 0 to 0.7
 
 // OctoWS2811 objects
-const int ledsPerPin = matrix_width * matrix_height / 8;
-DMAMEM int displayMemory[ledsPerPin * 6];
-int drawingMemory[ledsPerPin * 6];
+const int bytesPerLed = 3;
+const int ledsPerStrip = matrix_width * matrix_height / 8;
+const int numPins = 8;
+byte pinList[numPins] = {2, 3, 4, 5, 6, 7, 8, 9};
+
+DMAMEM int displayMemory[ledsPerStrip * numPins * bytesPerLed / 4];
+int drawingMemory[ledsPerStrip * numPins * bytesPerLed / 4];
 const int config = WS2811_GRB | WS2811_800kHz;
-OctoWS2811 leds(ledsPerPin, displayMemory, drawingMemory, config);
-CRGB fast_leds[ledsPerPin * 6];
+OctoWS2811 leds(ledsPerStrip, displayMemory, drawingMemory, config);
+CRGB fast_leds[ledsPerStrip * numPins * bytesPerLed / 4];
 CTeensy4Controller<RGB, WS2811_800kHz> *pcontroller;
 
 // Audio library objects
@@ -101,6 +104,35 @@ void computeVerticalLevels()
   }
 }
 
+// setting up the code for the 2 potentiometers
+const int POT_PIN_SENSATIVITY = 3;
+const int POT_PIN_COLOR = 4;
+float prevPotValSensativity = maxLevel;
+float colorRangeFactor = 1;
+
+int analogReadZeroToOne(int pin)
+{
+  int val = analogRead(pin);
+  float rangeVal = val / 1024; // 10bit sample
+  return rangeVal;
+}
+
+void ApplyPotentiometerSettings()
+{
+  // read the potentiometer values
+  int potValSensativity = analogReadZeroToOne(POT_PIN_SENSATIVITY);
+  int potValColor = analogReadZeroToOne(POT_PIN_COLOR);
+
+  // apply the potentiometer values
+  if (fabsf(prevPotValSensativity - potValSensativity) > 0.001)
+  {
+    maxLevel = potValSensativity;
+    computeVerticalLevels();
+    prevPotValSensativity = potValSensativity;
+  }
+  colorRangeFactor = potValColor;
+}
+
 // Run setup once
 void setup()
 {
@@ -141,20 +173,26 @@ const int LevelColorInfluant[matrix_width] = {
   8,8,8,8,8,8,8,
   6,6,6,6,6,6,6,
   6,6,6,6,6,6,6,
-  3,3,3,3,3,3,3,
-  3,3,3,3,3,3,3,
-  0,0
+  4,4,4,4,4,4,4,
+  4,4,4,4,4,4,4,
+  3,0
   };
 int calcNextStepColor(int allLevels[matrix_width], float hue)
 {
   int nextHue = 0;
   for (int i = 0; i < matrix_width; i++)
   {
-    nextHue += minMaxNormalization(allLevels[i], 0, matrix_height, 0,  LevelColorInfluant[i]);
+    nextHue += minMaxNormalization(allLevels[i]*0.6, 0, matrix_height, 0,  LevelColorInfluant[i]);
   }
+
+  nextHue = (colorRangeFactor * nextHue) + (1.0 - colorRangeFactor) * hue;
   
-  if (nextHue > 360){
-    nextHue = 360;
+  if (nextHue > FadeMaxH){
+    nextHue = FadeMaxH;
+  }
+
+   if (nextHue < FadeMinH){
+    nextHue = FadeMinH;
   }
 
   return nextHue;
@@ -178,7 +216,7 @@ void getHueFromOctava()
 
   // Serial.print(prevOctavaArr[i]);
   // Serial.print(" ");
-  if (levelOct > thresholdVertical[10])
+  if (levelOct > thresholdVertical[14])
   {
     currH = currH + 0.001;
     if (currH >= FadeMaxH)
@@ -213,6 +251,8 @@ void loop()
   float level;
   int allLevelsPassThreshold[matrix_width] = {0};
 
+  // ApplyPotentiometerSettings();
+
   if (fft.available())
   {
     // freqBin counts which FFT frequency data has been used,
@@ -235,13 +275,13 @@ void loop()
         // for each vertical pixel, check if above the threshold
         // and turn the LED on or off
         int color;
-        if (level >= thresholdVertical[15])
+        if (level >= thresholdVertical[y])
         {
           allLevelsPassThreshold[x] += 1;
           currV = FadeMaxV;
           color = HSVtoRGB(currH, FadeMaxS, FadeMaxV);
-          Serial.print(color);
-          Serial.print(" ");
+          // Serial.print(currH);
+          // Serial.print(" ");
           leds.setPixel(xy(x, y), color);
         }
         else
@@ -254,11 +294,11 @@ void loop()
           }
 
           color = HSVtoRGB(currH, FadeMinS, FadeMinV);
-          Serial.print(color);
-          Serial.print(" ");
+          // Serial.print(color);
+          // Serial.print(" ");
           leds.setPixel(xy(x, y), color);
         }
-        Serial.println(" ");
+        // Serial.println(" ");
       }
       // Serial.println(" ");
       // increment the frequency bin count, so we display
@@ -267,9 +307,11 @@ void loop()
     }
 
     currH = calcNextStepColor(allLevelsPassThreshold, currH);
+    Serial.print(currH);
+    Serial.println(" ");
     // after all pixels set, show them all at the same instant
     leds.show();
     // FastLED.show();
-    //  Serial.println();
+    // Serial.println();
   }
 }
